c844c3ae13cadb6f70593b8a420ef7f1fadb5d71
Shinya Kumagai <roomful.rooms@gmail.com>
2016-07-31 19:47:59 +0900

    Added activities for edit toggle

---
+++ app/src/main/kotlin/com/droibit/autoggler/edit/add/AddToggleActivity.kt
@@ -0,0 +1,13 @@
+package com.droibit.autoggler.edit.add
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.droibit.autoggler.R
+
+class AddToggleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_toggle)
+    }
+}
---
+++ app/src/main/kotlin/com/droibit/autoggler/edit/update/UpdateToggleActivity.kt
@@ -0,0 +1,13 @@
+package com.droibit.autoggler.edit.update
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.droibit.autoggler.R
+
+class UpdateToggleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_toggle)
+    }
+}
