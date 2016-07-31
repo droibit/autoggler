e4c9fdf41a202a1bfc1fba3a7c0e34d88a25d5cd
Shinya Kumagai <roomful.rooms@gmail.com>
2016-07-31 19:19:12 +0900

    Changed to TogglesActivity from MainActivity

--- app/src/main/kotlin/com/droibit/autoggler/toggles/TogglesActivity.kt
+++ app/src/main/kotlin/com/droibit/autoggler/toggles/TogglesActivity.kt
@@ -1,12 +1,13 @@
-package com.droibit.autoggler
+package com.droibit.autoggler.toggles
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.droibit.autoggler.R
 
-class MainActivity : AppCompatActivity() {
+class TogglesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_toggles)
     }
 }
