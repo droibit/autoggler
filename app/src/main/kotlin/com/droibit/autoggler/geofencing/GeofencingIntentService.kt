2cbe292e916dd0ce45a3122bb0bfcc3a3b283db3
Shinya Kumagai <roomful.rooms@gmail.com>
2016-07-31 20:30:50 +0900

    Added GeofencingIntentService class

---
+++ app/src/main/kotlin/com/droibit/autoggler/geofencing/GeofencingIntentService.kt
@@ -0,0 +1,12 @@
+package com.droibit.autoggler.geofencing
+
+import android.app.IntentService
+import android.content.Intent
+
+
+class GeofencingIntentService : IntentService(GeofencingIntentService::class.java.simpleName) {
+
+    override fun onHandleIntent(intent: Intent?) {
+
+    }
+}
