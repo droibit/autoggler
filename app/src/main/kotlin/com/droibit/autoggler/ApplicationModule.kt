8a3e8e4375bbfc9c038d4ed1bfdf987e3837e77e
Shinya Kumagai <roomful.rooms@gmail.com>
2016-08-07 01:31:20 +0900

    Added provide CompositeSubscription to ApplicationModule

--- app/src/main/kotlin/com/droibit/autoggler/ApplicationModule.kt
+++ app/src/main/kotlin/com/droibit/autoggler/ApplicationModule.kt
@@ -4,16 +4,16 @@ import android.content.Context
 import com.droibit.autoggler.utils.EmptyTimberTree
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.provider
 import com.github.salomonbrys.kodein.singleton
+import rx.subscriptions.CompositeSubscription
 import timber.log.Timber
 
 fun applicationModule(context: Context, debuggable: Boolean) = Kodein.Module {
 
     bind<Context>() with instance(context)
 
-    if (debuggable) {
-        bind<Timber.Tree>() with singleton { Timber.DebugTree() }
-    } else {
-        bind<Timber.Tree>() with singleton { EmptyTimberTree() }
-    }
+    bind<Timber.Tree>() with singleton { if (debuggable) Timber.DebugTree() else EmptyTimberTree() }
+
+    bind<CompositeSubscription>() with provider { CompositeSubscription() }
 }
