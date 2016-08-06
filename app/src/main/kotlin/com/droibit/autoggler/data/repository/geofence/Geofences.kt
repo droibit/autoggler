500f9f26a01a0bf699857f65492f931fe23a6e7c
Shinya Kumagai <roomful.rooms@gmail.com>
2016-08-06 20:33:19 +0900

    Added toString method to models

--- app/src/main/kotlin/com/droibit/autoggler/data/repository/geofence/Geofences.kt
+++ app/src/main/kotlin/com/droibit/autoggler/data/repository/geofence/Geofences.kt
@@ -8,16 +8,29 @@ open class Circle(
         open var lat: Double = 0.0,
         open var lng: Double = 0.0,
         open var radius: Double = 0.0
-) : RealmObject()
+) : RealmObject() {
+    override fun toString(): String{
+        return "Circle(lat=$lat, lng=$lng, radius=$radius)"
+    }
+}
 
 open class Trigger(
         open var toggleWifi: Boolean = false,
         open var toggleVibration: Boolean = false
-) : RealmObject()
+) : RealmObject() {
+    override fun toString(): String{
+        return "Trigger(toggleWifi=$toggleWifi, toggleVibration=$toggleVibration)"
+    }
+}
 
 open class Geofence(
         @PrimaryKey open var id: Long = 0L,
         @Required open var name: String = "",
         open var circle: Circle = Circle(),
         open var trigger: Trigger = Trigger()
-) : RealmObject()
+) : RealmObject() {
+
+    override fun toString(): String{
+        return "Geofence(id=$id, name='$name', circle=$circle, trigger=$trigger)"
+    }
+}
