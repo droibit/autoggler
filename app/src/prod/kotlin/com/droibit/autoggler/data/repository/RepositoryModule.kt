ebfd8e495ce53c2da36db40834aae67fce0882b3
Shinya Kumagai <roomful.rooms@gmail.com>
2016-08-24 23:05:14 +0900

    Added to RepositoryModule for prod build

---
+++ app/src/prod/kotlin/com/droibit/autoggler/data/repository/RepositoryModule.kt
@@ -0,0 +1,43 @@
+package com.droibit.autoggler.data.repository
+
+import android.content.Context
+import com.droibit.autoggler.data.repository.geofence.GeofenceRepository
+import com.droibit.autoggler.data.repository.geofence.GeofenceRepositoryImpl
+import com.droibit.autoggler.data.repository.source.AutoIncrementor
+import com.droibit.autoggler.data.repository.source.GeofencePersistenceContract.COLUMN_ID
+import com.droibit.autoggler.data.repository.source.RealmProvider
+import com.droibit.autoggler.data.repository.source.RealmProvider.Companion.FILE_NAME
+import com.droibit.autoggler.data.repository.source.RealmProviderImpl
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.provider
+import com.github.salomonbrys.kodein.singleton
+import io.realm.RealmConfiguration
+import rx.subscriptions.CompositeSubscription
+
+fun repositoryModule() = Kodein.Module {
+
+    bind<RealmConfiguration>() with singleton {
+        val context: Context = instance()
+        RealmConfiguration.Builder(context)
+                .name(FILE_NAME)
+                .build()
+    }
+
+    bind<RealmProvider>() with singleton { RealmProviderImpl(instance()) }
+
+    bind<AutoIncrementor>("geofence") with singleton { AutoIncrementor(COLUMN_ID) }
+
+    bind<GeofenceRepository>() with singleton {
+        GeofenceRepositoryImpl(instance(), instance("geofence")).apply {
+            _deleteGeofences()
+        }
+    }
+
+//    bind<LocationRepository>() with singleton { Mock.LocationRepositoryImpl() }
+
+    bind<CompositeSubscription>() with provider { CompositeSubscription() }
+}
+
+
+
