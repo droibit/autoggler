6e801950cb4565c7b702a744faf1831168dfc1c9
Shinya Kumagai <roomful.rooms@gmail.com>
2016-08-31 12:13:16 +0900

    Fixed release build error

--- app/src/release/java/com/droibit/autoggler/stetho/Stetho.kt
+++ app/src/release/java/com/droibit/autoggler/stetho/Stetho.kt
@@ -1,10 +1,5 @@
 package com.droibit.autoggler.stetho
 
-import android.content.Context
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider
-import java.util.regex.Pattern
-import com.facebook.stetho.Stetho as RawStetho
-
 class Stetho() {
 
     fun initialize() {
