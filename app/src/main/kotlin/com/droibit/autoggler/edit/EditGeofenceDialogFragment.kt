553604903c9aae5951cad0945166238b021dc099
Shinya Kumagai <roomful.rooms@gmail.com>
2016-09-04 14:16:11 +0900

    Changed EditGeofenceContentView to static inner class

--- app/src/main/kotlin/com/droibit/autoggler/edit/EditGeofenceDialogFragment.kt
+++ app/src/main/kotlin/com/droibit/autoggler/edit/EditGeofenceDialogFragment.kt
@@ -1,14 +1,21 @@
 package com.droibit.autoggler.edit
 
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AlertDialog
-import android.widget.Button
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import android.widget.*
 import com.droibit.autoggler.R
 import com.droibit.autoggler.data.repository.geofence.Geofence
+import com.github.droibit.chopstick.bindView
+import com.linearlistview.LinearListView
 
 class EditGeofenceDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
 
@@ -35,11 +42,52 @@ class EditGeofenceDialogFragment : DialogFragment(), DialogInterface.OnClickList
         }
     }
 
+
+    internal class EditGeofenceContentView : LinearLayout {
+
+        private val geofenceNameView: EditText by bindView(R.id.geofence_name)
+
+        private val geofenceRadiusView: Spinner by bindView(R.id.geofence_radius)
+
+        private val toggleListView: LinearListView by bindView(R.id.toggle_list)
+
+        @JvmOverloads
+        constructor(context: Context,
+                    attrs: AttributeSet? = null,
+                    defStyleAttr: Int = 0) : super(context, attrs, defStyleAttr) {
+            View.inflate(context, R.layout.view_edit_geofence, this)
+
+            geofenceRadiusView.adapter = ArrayAdapter.createFromResource(
+                    context, R.array.edit_geofence_circle_radius_labels, R.layout.list_item_geofence_radius)
+        }
+
+        fun init(srcGeofence: Geofence) {
+            toggleListView.adapter = ToggleAdapter(context, srcGeofence)
+        }
+
+        inline fun onTextChanged(crossinline callback: (CharSequence?) -> Unit) {
+            geofenceNameView.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    callback(s)
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                }
+            })
+        }
+
+        fun hasGeofenceName() = !geofenceNameView.text.isNullOrEmpty()
+    }
+
+    private val positiveButton: Button by lazy { (dialog as AlertDialog).getButton(DialogInterface.BUTTON_POSITIVE) }
+
     private lateinit var contentView: EditGeofenceContentView
 
     private lateinit var geofence: Geofence
 
-    private lateinit var positiveButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,15 +100,19 @@ class EditGeofenceDialogFragment : DialogFragment(), DialogInterface.OnClickList
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val dialog = AlertDialog.Builder(context).run {
+            val self = this@EditGeofenceDialogFragment
             setTitle(R.string.edit_geofence_dialog_title)
-            setPositiveButton(R.string.update, this@EditGeofenceDialogFragment)
-            setNegativeButton(android.R.string.cancel, this@EditGeofenceDialogFragment)
+            setPositiveButton(R.string.update, self)
+            setNegativeButton(android.R.string.cancel, self)
 
-            contentView = EditGeofenceContentView(context).apply { init(geofence) }
+            contentView = EditGeofenceContentView(context).apply {
+                init(geofence)
+                onTextChanged { positiveButton.isEnabled = !it.isNullOrEmpty() }
+            }
             setView(contentView)
 
             if (arguments.getBoolean(ARG_DELETABLE, false)) {
-                setNeutralButton(R.string.delete, this@EditGeofenceDialogFragment)
+                setNeutralButton(R.string.delete, self)
             }
             create()
         }
@@ -71,7 +123,7 @@ class EditGeofenceDialogFragment : DialogFragment(), DialogInterface.OnClickList
     override fun onResume() {
         super.onResume()
 
-        positiveButton = (dialog as AlertDialog).getButton(DialogInterface.BUTTON_POSITIVE)
+        positiveButton.isEnabled = contentView.hasGeofenceName()
     }
 
     override fun onDestroyView() {
